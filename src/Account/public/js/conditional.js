8579e9ca148dd6986a4bb74a287c7aff91ea17a6
Ryan Mahoney <ryan@virtuecenter.com>
2014-11-28 12:10:53 -0500

    sometimes, don't require css

--- src/Account/public/js/conditional.js
+++ src/Account/public/js/conditional.js
@@ -4,14 +4,14 @@ if ($('.account-section').length) {
         var $ = require('jquery');
         require('semantic');
         require('behaviors.js');
-        var cssPath = '../css/style.js';
         if ($('#opine-account-config').length) {
             var config = JSON.parse($('#opine-account-config').text());
-            if (typeof(config['cssPath']) !== 'undefined')
-                cssPath = config['cssPath'];
+            if (typeof(config['cssPath']) == 'undefined')
+                require('../css/style.css');
             }
+        } else {
+            require('../css/style.css');
         }
-        require(cssPath);
     });
 } else {
     console.log("Skipping Loading Opine Account SSO");
