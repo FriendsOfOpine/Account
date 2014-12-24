ce5b52b9bb8dcb74cbe7d0939aee1cd555267989
Ryan Mahoney <ryan@virtuecenter.com>
2014-12-24 16:25:09 -0500

    embrace new standards

--- tests/AccountTest.php
+++ tests/AccountTest.php
@@ -6,7 +6,7 @@ use Opine\Container\Service as Container;
 class AccountTest extends PHPUnit_Framework_TestCase {
     public function setup () {
         $root = __DIR__ . '/../public';
-        $container = Container::instance($root, $root . '/../config/container.yml');
+        $container = Container::instance($root, $root . '/../config/containers/test-container.yml');
     }
 
     public function testSample () {
